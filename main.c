f86e500e91b515fbfa595b02b714b7e0e8f6d1ee
Mariano Ponti <nanogp@users.noreply.github.com>
2018-06-01 23:45:25 -0300

    informe de repositorios y final

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//ORIGEN CON TODA SU HITORIA EN http://github.com/nanogp/TP3.git
+//COPIA EN https://github.com/nanogp/Programacion1y-laboratorio1.git
+//OTRA COPIA EN https://github.com/nanogp/tp_laboratorio_1.git
 /**************************** INCLUSION DE LIBRERIAS PERSONALES **********************************/
 #include "General.h"
 #include "Menu.h"
@@ -10,27 +13,29 @@
 
 /**************************** MENUS **************************************************************/
 #define MAIN_MENU_TITULO "TP3 MARIANO PONTI"
-#define MAIN_MENU_CANT 6
+//cambiar MAIN_MENU_CANT a 6 y descomentar las opciones del menu para listar por pantalla
+#define MAIN_MENU_CANT 5
 #define MAIN_MENU_DETALLE1  "1. Agregar pelicula"
 #define MAIN_MENU_DETALLE2  "2. Borrar pelicula"
 #define MAIN_MENU_DETALLE3  "3. Modificar pelicula"
 #define MAIN_MENU_DETALLE4  "4. Generar pagina web"
-#define MAIN_MENU_DETALLE5  "5. Listado en pantalla"
-#define MAIN_MENU_DETALLE0  "0. Salir"
+#define MAIN_MENU_DETALLE5  "5. Salir"
+#define MAIN_MENU_DETALLE0  "0. Listado en pantalla"
 
 int main()
 {
    eMenu menuPrincipal = {/*titulo del menu*/{MAIN_MENU_TITULO},
                           /*cantidad de opciones*/MAIN_MENU_CANT,
-                          /*codigos*/{1,2,3,4,
-                                      //5,
-                                      0},
+                          /*codigos*/{1,2,3,4,5,
+                                      //0
+                                      },
                           /*descripciones*/{MAIN_MENU_DETALLE1,
                                             MAIN_MENU_DETALLE2,
                                             MAIN_MENU_DETALLE3,
                                             MAIN_MENU_DETALLE4,
-                                            //MAIN_MENU_DETALLE5,
-                                            MAIN_MENU_DETALLE0}};
+                                            MAIN_MENU_DETALLE5,
+                                            //MAIN_MENU_DETALLE0
+                                            }};
    int opcion;
    char salirDelPrograma = 'N';
 
@@ -73,12 +78,12 @@ int main()
             eArchivoHtml_generarWeb(listadoPeliculas, PELICULA_CANT_MAX);
             break;
          case 5:
-            /************ LISTADO EN PANTALLA ****************************************************/
-            eMovie_mostrarListado(listadoPeliculas, PELICULA_CANT_MAX);pausa();
-            break;
-         case 0:
             /************ SALIR DEL PROGRAMA *****************************************************/
             salirDelPrograma = pedirConfirmacion(MSJ_SALIR_DEL_PROGRAMA);
+            break;
+         case 0:
+            /************ LISTADO EN PANTALLA ****************************************************/
+            eMovie_mostrarListado(listadoPeliculas, PELICULA_CANT_MAX);pausa();
             break;
       }
 
